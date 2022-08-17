df34a6758a8d2ed0cda3474f38ee50a3e59d1421
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-17 16:29:38 +0530

    [Ganapati] Generics Integer value

---
+++ Generics/FindMax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMax
+    {
+        public int FindMaxInteger(int First, int Second, int Third)
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                return First;
+            }
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                return Second;
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+
+                return Third;
+            }
+            return 0;
+        }
+        
+
+    }
+}
+
+
---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            FindMax obj1 = new FindMax();
+            while (check)
+            {
+                Console.Write("Select Option:\n" +
+                    "1)Find Integer Maximum Number\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        int result = obj1.FindMaxInteger(4, 3, 2);
+                        Console.WriteLine(result);
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Maximum Number is:" + result);// To display Maximum Number
+                        Console.WriteLine("-----------------------------------");
+                        break;
+                    default:
+                        check = !check;
+                        break;
+                }
+            }
+        }
+    }
+}
